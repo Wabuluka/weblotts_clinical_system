67f4dca3873212993e2b30ad4ab973dac2103149
Wabuluka <dwabuluka@gmail.com>
2019-01-08 12:30:32 +0300

    [ft #163014655] post registra

--- api/clinicRegistrationAPI/app/controller/clinic_registra_view.py
+++ api/clinicRegistrationAPI/app/controller/clinic_registra_view.py
@@ -7,15 +7,20 @@ from app.models.clinic_registra import ClinicRegistra
 registras = []
 
 class ClinicRegistraView(MethodView):
-
+    
     def post(self):
-       data = request.json()
-       if data:
-           registra = ClinicRegistra(registraId = data["registraId"],
-                firstName = data["firstName"],
-                lastName = data["lastName"],
-                gender = data["gender"],
-                email = data["email"],
-                telephone =data["telephone"])
-           registras.append(registra)
-           return jsonify({"data" : registra})
+        data = request.get_json()
+       
+    
+        registra = ClinicRegistra(
+            registraId = data['registraId'],
+            firstName = data['firstName'],
+            lastName = data['lastName'],
+            gender = data['gender'],
+            email = data['email'],
+            telephone =data['telephone'])
+
+        registras.append(registra)
+        return jsonify({"data" : registra.registra_details_to_dictionary()})
+
+   
